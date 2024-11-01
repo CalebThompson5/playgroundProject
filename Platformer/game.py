32ec6d8486c6efba94c1ec5a108cf7594e5dea4d
cjthompson5 <cjthompson5@crimson.ua.edu>
2024-11-01 18:08:59 -0500

    Adding asset pack to project structure

--- Platformer/game.py
+++ Platformer/game.py
@@ -4,28 +4,31 @@
 import sys
 import pygame
 
-# Initializing pygame
-pygame.init()
+class Game:
+    def __init__(self) -> None:
+        # Initializing pygame
+        pygame.init()
 
-# Here we are setting the panel for the game. The resolution (in pixels) is being passed in as a tuple
-screen = pygame.display.set_mode((640, 480))
-pygame.display.set_caption("Platformer Game")
+        # Here we are setting the panel for the game. The resolution (in pixels) is being passed in as a tuple
+        self.screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption("Platformer Game")
 
-# Creating a clock so we can set a frame rate. We are gonna use a fixed frame rate (60fps) so the game doesn't eat CPU
-clock = pygame.time.Clock()
+        # Creating a clock so we can set a frame rate. We are gonna use a fixed frame rate (60fps) so the game doesn't eat CPU
+        self.clock = pygame.time.Clock()
 
-# We are going to use one loop for physics, rendering, etc. 
-while True:
-    # Loop for input handling. Application will not respond without this loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    def run(self):
+        # We are going to use one loop for physics, rendering, etc. 
+        while True:
+            # Loop for input handling. Application will not respond without this loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    # This line refreshes the screen. Failing to include this line will leave the screen black
-    pygame.display.update()
+            # This line refreshes the screen. Failing to include this line will leave the screen black
+            pygame.display.update()
 
-    # Setting loop to 60 fps
-    clock.tick(60)
+            # Setting loop to 60 fps
+            self.clock.tick(60)
 
-    
+Game().run()
